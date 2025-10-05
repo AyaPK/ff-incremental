776bc4fb2f2b7740b2fb9df25a912db20cb711bc
AyaPK <Sherbnya@gmail.com>
2025-10-05 22:26:00 +0100

    Adds exploration mechanic to main scene
    Introduces an exploration mechanic, adding a new button to
    initiate the battle scene.
    
    Refactors the main scene to incorporate this functionality and
    removes unused resources.
    
    The battle scene is now hidden by default and only displayed upon
    user interaction.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,25 +1,28 @@
 extends Node2D
 
+var state: String
+
 func _ready() -> void:
 	BattleManager.load_player()
-	_on_show_battle_pressed()
+	_on_hide_battle_pressed()
 
 func _process(_delta: float) -> void:
 	pass
 
-
 func _on_hide_battle_pressed() -> void:
+	state = "hide"
 	$BattleScene.process_mode = Node.PROCESS_MODE_DISABLED
 	$BattleScene.hide()
 
-
-func _on_show_battle_pressed() -> void:
-	$BattleScene.process_mode = Node.PROCESS_MODE_ALWAYS
-	$BattleScene.refresh_stats()
-	$BattleScene.change_area("nearby_plains")
-	$BattleScene.character_slot_1.move_timer.start()
-	$BattleScene.enemy_slot_1.move_timer.start()
-	$BattleScene.show()
+func _on_explore_pressed() -> void:
+	if state != "explore":
+		state = "explore"
+		$BattleScene.process_mode = Node.PROCESS_MODE_ALWAYS
+		$BattleScene.refresh_stats()
+		$BattleScene.change_area("nearby_plains")
+		$BattleScene.character_slot_1.move_timer.start()
+		$BattleScene.enemy_slot_1.move_timer.start()
+		$BattleScene.show()
 
 
 func _on_heal_pressed() -> void:
